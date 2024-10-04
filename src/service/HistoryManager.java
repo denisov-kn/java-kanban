4fc007e46c0e02b16e466f621cc4b7b32625fbe6
DenisovKN <denisov.kn@yandex.ru>
2024-10-04 04:38:32 +0300

    feat: Поправить CodeStyle HistoryManager

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -1,9 +1,14 @@
 package service;
+
 import model.Task;
+
 import java.util.List;
 
 public interface HistoryManager {
+
     <T extends Task> void add(T task);
+
     List<Task> getHistory();
+
     void remove(int id);
 }
